25ddfea149ef938533cf5cec75cc848b6743682c
Gavin <gtjin1103@gmail.com>
2021-12-13 22:06:43 +0100

    hallo

--- assesment formulier/assesment_formulieren.sql
+++ assesment formulier/assesment_formulieren.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 08 dec 2021 om 10:33
--- Serverversie: 10.4.19-MariaDB
--- PHP-versie: 8.0.6
+-- Gegenereerd op: 13 dec 2021 om 22:05
+-- Serverversie: 10.4.21-MariaDB
+-- PHP-versie: 8.0.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -98,6 +98,45 @@ CREATE TABLE `assesment_form2` (
 
 INSERT INTO `assesment_form2` (`titel_AF2`, `tekst_intro`, `titel_checklist`, `veld1`, `veld2`, `veld3`, `checkboxen`, `titelRF2`, `tekst_uitleg`, `vormgeven_beoordeling`, `techniek_beoordeling`, `tools_beoordeling`, `softskills_beoordeling`, `AVO_beoordeling`, `bijzondere_kwaliteiten`, `titel_TF2`, `tekst1`, `tekst2`, `tekst3`, `titel_doorgroeiAdvies`, `handtekening_assesor`, `handtekening_student`) VALUES
 ('Afsluiting Fase 2', 'Dit formulier gaat je helpen om te komen tot een assessment voor fase 1. Als je deel A samen met je coach \r\nhebt doorlopen laat je op deel B de vakdocenten hun feedback invullen. Pas als beide delen compleet zijn\r\nkun je bij je coach het assessment indienen. Deze zal je voordragen en een terug koppeling geven als het\r\nassessment is bekeken en beoordeeld', 'Checklist', 'Student', 'Coach', 'Datum', 2, 'Review Fase 2', 'In te vullen door de vakdocent: <br>Maak een afspraak met de desbetrefende docent en laat hem de beoordeling en feedback invullen. \r\nBespreek deze zodat je ook begrijpt hoe de feedback tot stand is gekomen.', 'Vormgeven', 'Techniek', 'Tools', 'Softskills', 'AVO', 'Bijzondere kwaliteiten', 'Terug koppeling fase 2', 'Je hebt een beeld van het profiel webdesigner.', 'Je weet waar je staat t.o.v. het beeld van een web', 'Je bent op het niveau van een beginnend webdesigne', 'Doorgroei advies', 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `assesment_form4`
+--
+
+CREATE TABLE `assesment_form4` (
+  `titel_AF4` varchar(50) NOT NULL,
+  `tekst_intro` varchar(400) NOT NULL,
+  `titel_checklist` varchar(30) NOT NULL,
+  `student_input` varchar(30) NOT NULL,
+  `coach_input` varchar(30) NOT NULL,
+  `datum_input` varchar(30) NOT NULL,
+  `checkboxen` int(11) NOT NULL,
+  `titel_RF4` varchar(50) NOT NULL,
+  `tekst_intro_deel_B` varchar(400) NOT NULL,
+  `vormgeven_beoordeling` varchar(30) NOT NULL,
+  `techniek_beoordeling` varchar(30) NOT NULL,
+  `ondernemen_beoordeling` varchar(30) NOT NULL,
+  `softskills_beoordeling` varchar(30) NOT NULL,
+  `AVO_beoordeling` varchar(30) NOT NULL,
+  `evt_kwaliteiten` varchar(30) NOT NULL,
+  `TF3` varchar(50) NOT NULL,
+  `input_tekst_1_deel_c` varchar(100) NOT NULL,
+  `input_tekst_2_deel_c` varchar(100) NOT NULL,
+  `input_tekst_3_deel_c` varchar(100) NOT NULL,
+  `input_tekst_4_deel_c` varchar(100) NOT NULL,
+  `titel_doorgroei_advies` varchar(50) NOT NULL,
+  `handtekening_assesor` int(11) NOT NULL,
+  `handtekening_student` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `assesment_form4`
+--
+
+INSERT INTO `assesment_form4` (`titel_AF4`, `tekst_intro`, `titel_checklist`, `student_input`, `coach_input`, `datum_input`, `checkboxen`, `titel_RF4`, `tekst_intro_deel_B`, `vormgeven_beoordeling`, `techniek_beoordeling`, `ondernemen_beoordeling`, `softskills_beoordeling`, `AVO_beoordeling`, `evt_kwaliteiten`, `TF3`, `input_tekst_1_deel_c`, `input_tekst_2_deel_c`, `input_tekst_3_deel_c`, `input_tekst_4_deel_c`, `titel_doorgroei_advies`, `handtekening_assesor`, `handtekening_student`) VALUES
+('Afsluiting Fase 4', '', 'Checklist', 'Student', 'Coach', 'Datum', 0, 'Review Fase 4', '', 'Vormgeven', 'Techniek', 'Ondernemen', 'Softskills', 'AVO', 'Evt. bijzondere kwaliteiten', 'Terug koppelings fase 3', '', '', '', '', 'Doorgroei advies', 0, 0);
 
 -- --------------------------------------------------------
 
