6c97d937479c195166ccc3b19f0a8c93e7402e2f
constantijn <constantijn@vanhartesveldt.nl>
2022-01-25 13:42:56 +0100

    .env support toegevoegd voor de db

--- code/core/databaseConnection.php
+++ code/core/databaseConnection.php
@@ -1,33 +1,40 @@
 <!-- verbinding maken met database -->
 <?php
+require_once "../../vendor/autoload.php";
+$dotenv = Dotenv\Dotenv::createImmutable("../../");
+$dotenv->load();
 class Database{
-   private $host = "localhost";
-   private $db_name = "assesment_formulieren";
-   private $username = "root";
-   private $password = "";
+   // private $host;
+   // private $db_name;
+   // private $username;
+   // private $password;
    public $con;
 
    public function __construct(){
-    $this->getConnection();
+      // var_dump($_ENV);
+
+
+    $this->getConnection($_ENV["host"],$_ENV["username"],$_ENV["password"],$_ENV["db"]);
    }
    
-   public function getConnection(){
+   public function getConnection($host,$username,$password,$db_name){
       $this->con = null;
       // echo "je bent verbonden met de database";
       // echo "<br>";
       try{
-         $this->con = new mysqli($this->host, $this->username, $this->password, $this->db_name);
+         $this->con = new mysqli($host, $username, $password, $db_name);
          
       }
       catch(Exception $e)
       {
          echo "Fout tijdens verbinden: " . $e->getMessage();
       }
+      echo $con;
       return $this->con;
    }
 }
 
-// $db = new Database();
+$db = new Database();
 // $con = $db->getConnection();
 
 ?>
