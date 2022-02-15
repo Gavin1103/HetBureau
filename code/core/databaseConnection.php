a795ae0a94cf87e90d0829617a35de4b6e325a1c
constantijn <constantijn@vanhartesveldt.nl>
2022-02-15 08:41:43 +0100

    work

--- code/core/databaseConnection.php
+++ code/core/databaseConnection.php
@@ -4,23 +4,14 @@ require_once "../../vendor/autoload.php";
 $dotenv = Dotenv\Dotenv::createImmutable("../../");
 $dotenv->load();
 class Database{
-   // private $host;
-   // private $db_name;
-   // private $username;
-   // private $password;
    public $con;
 
    public function __construct(){
-      // var_dump($_ENV);
-
-
     $this->getConnection($_ENV["host"],$_ENV["username"],$_ENV["password"],$_ENV["db"]);
    }
    
    public function getConnection($host,$username,$password,$db_name){
       $this->con = null;
-      // echo "je bent verbonden met de database";
-      // echo "<br>";
       try{
          $this->con = new mysqli($host, $username, $password, $db_name);
          
