ddcb293ded1a13f8e5cb3c2f23d980ae8f60ef4b
Gavin1103 <gtjin1103@gmail.com>
2022-02-22 08:48:24 +0100

    laatste versie

--- code/formulieren/form.php
+++ code/formulieren/form.php
@@ -1,20 +1,52 @@
-<?php 
+<?php
 require "./get_formulier.php";
 $getForm = new GetFormFunction();
 $form_data = $getForm->get_formulieren(1);
 
-
 echo "<pre>";
-
-foreach($form_data as $key => $value) {
-    echo $value->vraag;
+foreach ($form_data as $key => $form) {
+    echo $form->titel;
     echo "<br/>";
-    foreach($value->antwoorden as $key2 => $value2) {
-        echo $value2->label;
+    foreach ($form->info as $key2 => $infoForm) {
+        echo $infoForm->beschrijving;
         echo "<br/>";
-        echo "<br>";    
     }
-
-
+    foreach ($form->info as $key2 => $infoForm) {
+        echo $infoForm->element;
+        echo "<br/>";
+    }
 }
+echo "<br>";
+
+foreach ($form_data as $key => $form) {
+    echo $form->titelChecklist;
+    echo "<br/>";
+    foreach ($form->infoChecklist as $key2 => $infoForm) {
+        echo $infoForm->beschrijvingChecklist;
+        echo "<br/>";
+    }
+}
+
+echo "<br>";
+
+foreach ($form_data as $key => $form) {
+    echo $form->naam_veld_student;
+    echo "<br/>";
+    foreach ($form->inputTypeStudent as $key2 => $infoForm) {
+        echo $infoForm->inputTypeText;
+        echo "<br/>";
+    }
+}
+
+foreach ($form_data as $key => $form) {
+    echo $form->coach_veld_student;
+    echo "<br/>";
+    foreach ($form->inputTypeText as $key2 => $infoForm) {
+        echo $infoForm->inputTypeText;
+        echo "<br/>";
+    }
+}
+
+
+
 
