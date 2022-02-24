9e7fef67032af9526d55bd57acd7f94178a1b3ec
constantijn <constantijn@vanhartesveldt.nl>
2022-02-24 14:42:48 +0100

    fixed dynamic forms

--- code/formulieren/form.php
+++ code/formulieren/form.php
@@ -4,12 +4,22 @@ $getForm = new GetFormFunction();
 $form_data = $getForm->get_formulieren(1);
 
 echo "<pre>";
-var_dump($form_data->coach);
+// var_dump($form_data->student->vraagen);
+// $kaas = json_decode($form_data);
 
-foreach ($form_data  as $key => $form) {
-    
-    echo $form->titel;
- 
+foreach ($form_data->student->vraagen  as $key => $form) {
+    // var_dump($form);
+    echo $form->vraag;
+    echo "<br/>";
+    foreach ($form->antwoorden as $key => $antwoord){
+        echo "<label>{$antwoord->label}?</label>";
+        echo "<input name='{$form->vraag} placeholder='{$form->vraag}'/>";
+    }
+    // echo "<input name='{$form->vraag} placeholder='{$form->vraag}'/>";
+
+    echo "<br/>";
+    echo "<br/>";
+
 }
 
 // foreach ($form_data as $key => $form) {
