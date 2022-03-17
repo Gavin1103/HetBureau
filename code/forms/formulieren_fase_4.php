088cc6b930c906c12911e9e31612abcc4d2c31cb
Gavin <gtjin1103@gmail.com>
2022-03-17 15:16:55 +0100

    checkboxen joinen werkt

--- code/forms/formulieren_fase_4.php
+++ code/forms/formulieren_fase_4.php
@@ -2,14 +2,19 @@
 
 include("../core/databaseConnection.php");
 include("./getForms/getFormFase4.php");
+include './getForms/getCheckBoxen.php';
 
 $database = new Database();
 
 $getForm = new retrieveFormFase4();
 $executeForm = $getForm->getFormFase4();
 
+
+$getCheckboxFunction = new checkboxen();
+$executeCheckboxen = $getCheckboxFunction->getCheckboxenForm1();
+
+
 echo "<pre>";
 var_dump($executeForm);
 
 //  echo $executeForm["coach"];
-?>
