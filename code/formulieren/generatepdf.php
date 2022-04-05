46c216b519f61a7fc4d28342caf42537ad1e6705
kanker kaas <kanker@kaas.nl>
2022-04-05 12:50:31 +0200

    pdf generatie toegevoegd

--- code/formulieren/generatepdf.php
+++ code/formulieren/generatepdf.php
@@ -1,34 +1,12 @@
 <?php
-ob_end_clean();
 require_once "../../vendor/autoload.php";   
-use Fpdf\Fpdf;
+$html2pdf = new \Spipu\Html2Pdf\Html2Pdf('P', 'A4', 'en');
 
-// haal en parse de data uit de post body
-$raw_http_body = file_get_contents('php://input');
-$post_body = json_decode($raw_http_body,JSON_PRETTY_PRINT);   
+// haal post body op
+$http_body = file_get_contents('php://input');
 
-// Instantiate and use the FPDF class 
-$pdf = new FPDF();
-  
-//Add a new page
-$pdf->AddPage();
-  
-// Set the font for the text
-$pdf->SetFont('Arial', 'B', 12);
+// schrijf post body naar het pdf
+$html2pdf->writeHTML($http_body);
 
-$row = 20;
-foreach ($post_body as $key => $antwoord){
-    $vraag = $antwoord["label"];
-    $user_antwoord = $antwoord["answer"];
-    $pdf->Cell(20,20,"{$key} {$vraag}: {$user_antwoord}");
-    $pdf->Ln();
-
-    $row++;
-}
-
-// return the generated output
-$pdf->Output();
-
-// echo $pdf;
-
-?>
+// stuu de pdf 
+$html2pdf->output();
--- code/js/form.js
+++ code/js/form.js
@@ -3,42 +3,25 @@
  */
 
 const form = document.querySelector("form");
-console.log(form);
-
-let data = [];
 
 form.addEventListener("submit", async (event) => {
-  data = [];
   event.preventDefault();
 
-  // ga door alle html elementen in de form
-  for (let item of form) {
-    console.log(item);
-    // controleer of de element een input is
-    if (item.name) {
-      const element = {
-        answer: item.value,
-        label: item.name,
-      };
-      data.push(element);
-    }
-  }
-
-  console.log(data);
+  // haal alle html op 
+  const pdf = document.getElementById("pdf").innerHTML;
 
   // verstuur de geformateerde data en download de pdf daarna
   await fetch("/formulieren/generatepdf.php", {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: JSON.stringify(data),
+    
+    // stuur de opgehaalde html op
+    body: JSON.stringify(pdf),
   })
     .then((res) => res.blob())
     .then((blob) => {
+      // voeg de opgehaalde pdf op en toon dit in de browser
       const file = window.URL.createObjectURL(blob);
-      //   window.location.assign(file);
+      window.location.assign(file);
       console.log(blob);
     });
 });
