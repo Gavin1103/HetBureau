c5251c9b63500992d2d590296e9b142ccc56aff4
constantijn <constantijn@vanhartesveldt.nl>
2022-04-12 09:53:25 +0200

    added css stylesheets to pdf

--- code/formulieren/generatepdf.php
+++ code/formulieren/generatepdf.php
@@ -1,9 +1,16 @@
 <?php
+
+use Spipu\Html2Pdf\Html2Pdf;
+
 require_once "../../vendor/autoload.php";   
 $html2pdf = new \Spipu\Html2Pdf\Html2Pdf('P', 'A4', 'en');
 
 // haal post body op
 $http_body = file_get_contents('php://input');
+
+// write css to pdf
+$html2pdf->writeHTML('<link rel="stylesheet" type="text/css" href="style.css">');
+
 
 // schrijf post body naar het pdf
 $html2pdf->writeHTML($http_body);
